92f7c59fb194c1a6e953e46fd2d8e1333620a94d
andrew.yang <yyj_890627@163.com>
2016-11-29 15:21:43 +0800

    statr game
    statr game

---
+++ luck_runnum/js/yyj_luckStart.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: anchen
+ * @Date:   2016-11-29 14:02:12
+ * @Last Modified by:   anchen
+ * @Last Modified time: 2016-11-29 15:04:37
+ */
+'use strict';
+var goluck = function() {
+    var $wrap = $('.js-nums'),
+        nums;
+    $wrap[0] && $wrap.find('.num').each(function(i, o) {
+        nums = Math.round(Math.random()*9+1) * 49;
+        $(this).css("backgroundPositionY", "0px")
+        $(this).animate({
+            'backgroundPositionX': '0px',
+            'backgroundPositionY': '-' + nums + 'px'
+        }, 500);
+    });
+}
